179d30bfe56b95586e270e3d3ce5d1127ddd1ce5
Ronaldinho7670 <hss1993@abv.bg>
2023-12-21 10:34:55 +0200

    fixed forgoten space

--- src/components/SearchData.jsx
+++ src/components/SearchData.jsx
@@ -5,7 +5,7 @@ function SearchData({EndSearchData}) {
             {EndSearchData.length > 0 ? 
                 <div className="search_data">
                     <h2 className="search_h2">{EndSearchData[1]} with ID: {EndSearchData[0]} has 
-                    {EndSearchData[1] === 'Project' ? 'been': ''} worked on {EndSearchData[1] === 'Project' ? 'by': ''}:</h2>
+                    {EndSearchData[1] === 'Project' ? ' been': ''} worked on {EndSearchData[1] === 'Project' ? 'by': ''}:</h2>
                     <ul className="search_list">
                         {EndSearchData[2].map(el => 
                             <li key={el[0]}>{EndSearchData[1] === 'Project' ? 'Employee' : 'Project'} with ID: 
